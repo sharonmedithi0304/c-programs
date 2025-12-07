32a05098955a4993ecc0941a7d4a2966ce7a9784
sharonmedithi0304 <sharonmedithi23@gmail.com>
2025-12-07 20:13:08 +0530

    Add files via upload

---
+++ sum_mat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3], b[3][3], sum[3][3];
+    int i, j;
+
+    printf("Enter 9 values for first 3x3 matrix:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter 9 values for second 3x3 matrix:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("Sum of the matrices is:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
