28ecbf5a8ae4a5d8dcf870ad11ca0af34cc24004
sharonmedithi0304 <sharonmedithi23@gmail.com>
2025-12-08 02:22:14 +0530

    Add files via upload

---
+++ sum.mat,2darr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r, c, i, j, sum = 0;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            sum += a[i][j];
+        }
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
+
